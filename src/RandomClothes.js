26f65fda3d3c0827e7235e1f176299015204ca5b
hdjokic <hristtina99@gmail.com>
2022-02-11 19:54:45 -0600

    no two reds in one row allowed

--- src/RandomClothes.js
+++ src/RandomClothes.js
@@ -1,8 +1,11 @@
-function generateColor() {
+let dontRepeatDays = true;
+let dontDoubleRed = true;
+let isRed = false;
+function generateColor(dontInclude) {
     let availableColors = ["red", "blue", "black", "white"];
-    let color = availableColors[Math.floor(Math.random() * 4)];
-    let color2 = availableColors[Math.floor(Math.random() * 4)];
 
+    availableColors = availableColors.filter(color => color != dontInclude);
+    let color = availableColors[Math.floor(Math.random() * 4)];
 
     return color;
 }
@@ -11,7 +14,11 @@ function recolorCell() {
     const table = document.querySelector("table");
     for (let i = 1; i < table.rows.length; i++) {
         for (let j = 1; j < table.rows[i].cells.length; j++) {
-            (table.rows[i].cells[j]).style.backgroundColor = generateColor()
+            table.rows[i].cells[j].style.backgroundColor = generateColor()
+            if(table.rows[i].cells[1].style.backgroundColor==="red"){
+                table.rows[i].cells[j].style.backgroundColor = generateColor("red")
+
+            }
         }
     }
 }
